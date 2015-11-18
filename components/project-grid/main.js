fa24e2c4ebcf0d0c3603a46f2ea6f7b17798ad19
jamesstidard <jamesstidard@gmail.com>
2015-11-18 10:11:19 +0000

    IFPL Demo build

--- components/project-grid/main.js
+++ components/project-grid/main.js
@@ -19,9 +19,40 @@ var ProjectGrid = Vue.extend({
             return result;
         },
         total_for(provider) {
-          return this.project.scores.filter( score  => score.provider_id == provider.id )
-                                    .map   ( score  => { return parseFloat(score.score) ? parseFloat(score.score) : 0 } )
-                                    .reduce( (a, b) => a + b )
+            return this.project.scores.filter( score  => score.provider_id == provider.id )
+                                      .map   ( score  => { return parseFloat(score.score) ? parseFloat(score.score) : 0 } )
+                                      .reduce( (a, b) => a + b )
+        },
+        add_requirement() {
+            // get new requirement id - in no way actual solution hack job for demo
+            var latest_imp = this.project.requirements.reduce( (a, b) => {
+                return (a.id > b.id) ? a.id : b.id
+            }, {id: 0})
+            var latest_req = this.project.requirements.reduce( (a, b) => {
+                return (a.requirement_id > b.requirement_id) ? a.requirement_id : b.requirement_id
+            }, {id: 0})
+            var new_req_id = latest_req + 1
+            var new_imp_id = latest_imp + 1
+
+            var new_req = {
+                id: new_imp_id,
+                requirement_id: new_req_id,
+                requirement: '',
+                sort_order: this.project.requirements.length + 1
+            }
+            var new_scores = this.project.providers.map( provider => { 
+                return {
+                    provider_id: provider.id,
+                    requirement_id: new_req_id,
+                    score: 0
+                }
+            } )
+
+            this.project.requirements.push(new_req)
+            this.project.scores.push(...new_scores)
+
+            // wait for dom to render and focus new field
+            
         }
     },
     computed: {
