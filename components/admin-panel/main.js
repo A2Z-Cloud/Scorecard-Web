053e054180bbbb166b4da0aa49dba3dbccd9813e
jamesstidard <jamesstidard@gmail.com>
2016-01-21 10:47:05 +0000

    init admin panel

---
+++ components/admin-panel/main.js
@@ -0,0 +1,29 @@
+import './main.css!';
+import tmpl from './main-tmpl.html!text';
+import Vue from 'vue';
+
+export default Vue.extend({
+    template: tmpl,
+    props: [
+
+    ],
+    data() {
+        return {
+
+        }
+    },
+    methods:{
+
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    events: {
+
+    ready() {
+
+    }
+});
